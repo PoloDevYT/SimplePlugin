95adca386d1c175f7bff9bd7ab8d4afad7d62c11
PoloDevYT <57382861+PoloDevYT@users.noreply.github.com>
2025-12-14 03:45:52 -0300

    Add simple Paper greeting plugin

---
+++ src/main/java/com/example/simpleplugin/SimplePlugin.java
@@ -0,0 +1,36 @@
+package com.example.simpleplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+
+public class SimplePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("SimplePlugin iniciado! Use /saudacao para dizer olá.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SimplePlugin finalizado. Até logo!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("saudacao")) {
+            return false;
+        }
+
+        String nome = sender.getName();
+        String mensagem = String.format("Olá, %s! Obrigado por testar o SimplePlugin.", nome);
+
+        if (sender instanceof Player player) {
+            player.sendMessage(mensagem);
+        } else {
+            sender.sendMessage(mensagem);
+        }
+
+        return true;
+    }
+}
